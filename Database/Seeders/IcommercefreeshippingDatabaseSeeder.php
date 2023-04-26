4bdfdc4782f51748da0edec865de0df9f225f26e
Cristian Marin <marin101998@gmail.com>
2023-04-26 12:16:44 -0500

    +add | modules seeds by jobs

--- Database/Seeders/IcommercefreeshippingDatabaseSeeder.php
+++ Database/Seeders/IcommercefreeshippingDatabaseSeeder.php
@@ -5,19 +5,21 @@ namespace Modules\Icommercefreeshipping\Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Modules\Icommercefreeshiping\Database\Seeders\IcommercefreeshipingModuleTableSeeder;
-
+use Modules\Isite\Jobs\ProcessSeeds;
 
 class IcommercefreeshippingDatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-      $this->call(IcommercefreeshippingModuleTableSeeder::class);
-      $this->call(PaymentTableSeeder::class);
-        //$this->call(GeozoneTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Model::unguard();
+    ProcessSeeds::dispatch([
+      "baseClass" => "\Modules\Icommercefreeshipping\Database\Seeders",
+      "seeds" => ["IcommercefreeshippingModuleTableSeeder", "PaymentTableSeeder"]
+    ]);
+  }
 }
